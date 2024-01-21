bfb62fbacf260c95783c0c15fd289f363be2613f
Roman Roganovich <roman.roganovich@mail.ru>
2024-01-21 18:09:44 +0300

    add email smtp serice

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -8,6 +8,9 @@ use App\Repository\CommentRepository;
 use App\Services\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
@@ -16,19 +19,21 @@ use Symfony\Component\Workflow\WorkflowInterface;
 class CommentMessageHandler
 {
     public function __construct(
-        private EntityManagerInterface $entityManager,
-        private SpamChecker            $spamChecker,
-        private CommentRepository      $commentRepository,
-        private MessageBusInterface    $bus,
-        private WorkflowInterface      $commentStateMachine,
-        private ?LoggerInterface       $logger = null,
+        private EntityManagerInterface              $entityManager,
+        private SpamChecker                         $spamChecker,
+        private CommentRepository                   $commentRepository,
+        private MessageBusInterface                 $bus,
+        private WorkflowInterface                   $commentStateMachine,
+        private MailerInterface                     $mailer,
+        #[Autowire('%admin_email%')] private string $adminEmail,
+        private ?LoggerInterface                    $logger = null,
     )
     {
         /**
          * WorkflowInterface Имя аргумента $commentStateMachine,
          * которое должно состоять из названия бизнес-процесса (comment)
          * и его типа (state_machine).
-        */
+         */
     }
 
     public function __invoke(CommentMessage $message)
@@ -62,8 +67,13 @@ class CommentMessageHandler
         } elseif ($this->commentStateMachine->can($comment, 'publish')
             || $this->commentStateMachine->can($comment, 'publish_ham')
         ) {
-            $this->commentStateMachine->apply($comment, $this->commentStateMachine->can($comment, 'publish') ? 'publish' : 'publish_ham');
-            $this->entityManager->flush();
+            $this->mailer->send((new NotificationEmail())
+                ->subject('New comment posted')
+                ->htmlTemplate('emails/comment_notification.html.twig')
+                ->from($this->adminEmail)
+                ->to($this->adminEmail)
+                ->context(['comment' => $comment])
+            );
         } elseif ($this->logger) {
             $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
         }
